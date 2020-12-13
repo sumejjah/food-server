4c5648ee71dc1de975c27d920026b7c5bdbf1739
sumejjaha+1-at-587105673553 <sumejjaha@maestralsolutions.com>
2020-12-13 23:19:40 +0100

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const axios = require('axios');
+require("dotenv").config();
+
+const app = express();
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+//  ROUTES
+app.post('/search', async (req, res) => {
+  try {
+    const result = await axios.get(
+      `${process.env.ROOT_URL}/search?app_id=${
+        process.env.APPLICATION_ID
+      }&app_key=${encodeURIComponent(
+        process.env.API_KEY
+      )}&q=${req.body.q}`);
+
+      res.status(200).json(result.data);
+  }
+  catch(error) {
+    res.status(404).json({message: error.message});
+  }  
+})
+
+app.listen(8080);
